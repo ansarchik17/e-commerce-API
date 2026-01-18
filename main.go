b88010974a0526b3557b7b99a756c560df953eb5
Ansar <kelesansar62@gmail.com>
2026-01-18 19:47:58 +0500

    added main go file

---
+++ main.go
@@ -0,0 +1,87 @@
+package main
+
+import (
+	"context"
+	"e-commerce-API/config"
+	"e-commerce-API/handlers"
+	"e-commerce-API/repositories"
+
+	"github.com/gin-contrib/cors"
+	"github.com/gin-gonic/gin"
+	"github.com/jackc/pgx/v5/pgxpool"
+	"github.com/spf13/viper"
+)
+
+func main() {
+	r := gin.New()
+	corsConfig := cors.Config{
+		AllowAllOrigins: true,
+		AllowHeaders:    []string{"*"},
+		AllowMethods:    []string{"*"},
+	}
+	r.Use(cors.New(corsConfig))
+
+	err := loadConfig()
+	if err != nil {
+		panic(err)
+	}
+
+	conn, err := connectToDb()
+	if err != nil {
+		panic(err)
+	}
+	userRepository := repositories.NewAuthRepository(conn)
+	productRepository := repositories.NewProductRepository(conn)
+	userHandler := handlers.NewAuthHandler(userRepository)
+	productHandler := handlers.NewProductHandler(productRepository)
+	r.POST("/create/user", userHandler.RegisterUser)
+	r.POST("/signIn", userHandler.SignIn)
+	r.POST("/signOut", userHandler.SignOut)
+	r.POST("/create/product", productHandler.AddProduct)
+	r.Run(":8020")
+}
+
+func loadConfig() error {
+	viper.AutomaticEnv()
+	viper.SetConfigFile(".env")
+	if err := viper.BindEnv("APP_HOST"); err != nil {
+		viper.SetDefault("APP_HOST", ":8050")
+	}
+	if err := viper.BindEnv("DB_CONNECTION_STRING"); err != nil {
+		viper.SetDefault("DB_CONNECTION_STRING", "postgres://postgres:postgres@localhost:5432/postgres")
+	}
+	if err := viper.BindEnv("JWT_SECRET_KEY"); err != nil {
+		viper.SetDefault("JWT_SECRET_KEY", "supersecretkey")
+	}
+	if err := viper.BindEnv("JWT_EXPIRES_IN"); err != nil {
+		viper.SetDefault("JWT_EXPIRES_IN", "24h")
+	}
+	err := viper.ReadInConfig()
+	if err != nil {
+		return err
+	}
+
+	var mapConfig config.MapConfig
+	err = viper.Unmarshal(&mapConfig)
+	if err != nil {
+		return err
+	}
+
+	config.Config = &mapConfig
+
+	return nil
+}
+
+func connectToDb() (*pgxpool.Pool, error) {
+	conn, err := pgxpool.New(context.Background(), config.Config.DbConnectionString)
+	if err != nil {
+		return nil, err
+	}
+
+	err = conn.Ping(context.Background())
+	if err != nil {
+		return nil, err
+	}
+
+	return conn, nil
+}
